72d931d6057b4f7bcfb34a885e84ebdb08653ada
Vladislav Mironchuk <vlad.mironchuk.02@gmail.com>
2022-04-24 21:56:43 +0300

    add: modal and carousel

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
-const arrowRight = document.querySelector('.arrow:nth-child(2)');
-console.log(arrowRight);
+//logic conneted to a modal
+const buttons = document.querySelectorAll('.button');
+const background = document.querySelector('.background');
+const modal = document.querySelector('.modal');
+const cross = document.querySelector('.modal .cross');
+const cards = document.querySelector('.cards');
+
+buttons.forEach((item) => {
+  item.addEventListener('click', showModal);
+});
+
+cross.addEventListener('click', hideModal);
+
+function showModal() {
+  document.body.style.overflowY = 'hidden';
+  background.classList.remove('disable');
+  modal.classList.remove('disable');
+}
+
+function hideModal() {
+  document.body.style.overflowY = 'overlay';
+  background.classList.add('disable');
+  modal.classList.add('disable');
+}
+
+//logic connected to a carousel
+const arrows = document.querySelectorAll('.arrow');
+
+let counter = 0;
+
+arrows.forEach((item) => {
+  item.addEventListener('click', scrollCarousel);
+});
+
+function scrollCarousel(event) {
+  switch (event.target.dataset.direction) {
+    case 'left': {
+      if (counter === -3) return;
+      cards.style.transform = `translateX(${(412 / 1920) * 100 * --counter}vw)`;
+      break;
+    }
+    case 'right': {
+      if (counter === 0) return;
+      cards.style.transform = `translateX(${(412 / 1920) * 100 * ++counter}vw)`;
+    }
+  }
+}
